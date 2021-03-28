13fe89e40c472c5fb0eb195c45149554d1ce3e70
전인재 <jjaery@433.co.kr>
2021-03-28 18:05:46 +0900

    3주차 수업 내용 커밋

---
+++ 3주차/interface/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Character a = new Character();
+            a.damage = 20;
+            a.name = "플레이어A";
+            Character b = new Character();
+            b.damage = 5;
+            b.name = "플레이어B";
+
+            a.Attack(b);
+            b.Attack(a);
+        }
+    }
+
+    class Character
+    {
+        public string name;
+        public int hp = 100;
+        public int damage = 10;
+
+        public void Attack(Character target)
+        {
+            Console.WriteLine($"{this.name}가 {target.name}을 공격!");
+            Console.WriteLine($"{target.hp} -> {target.hp - this.damage} ({this.damage})");
+            target.Hitted(this.damage);
+        }
+
+        private void Hitted(int damage)
+        {
+            hp -= damage;
+        }
+    }
+}
---
+++ 3주차/interface/InterfacePractice/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace InterfacePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person me = new Person();
+            Calculator calc = new Calculator();
+
+            DoSomething(me);
+            DoSomething(calc);
+        }
+
+        static void DoSomething(iSum target)
+        {
+            int result = target.Sum(100, 100);
+            Console.WriteLine(result);
+        }
+    }
+
+    class Person : iSum
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b + 1;
+        }
+    }
+
+    class Calculator : iSum
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+
+    interface iSum
+    {
+        int Sum(int a, int b);
+    }
+}
